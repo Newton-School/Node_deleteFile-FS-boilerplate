c46d47a032f9080e0915aca352fbd02f38d06285
Jatin-Mankhand <jatin.mankhand@newtonschool.co>
2023-02-14 21:38:59 +0530

    Done

--- src/index.js
+++ src/index.js
@@ -3,6 +3,5 @@ const fileName = "myfile.txt";
 const deleteFile = async (fileName) => {
   // write code here
   // dont change function name
-  return await fs.unlink(fileName);
 };
 module.exports = { deleteFile };
